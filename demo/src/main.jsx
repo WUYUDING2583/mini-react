8152d75d2db3365fa7b7ee02a2172f2c18c5c9ee
YUDING WU <yudingwu@YUDINGs-MacBook-Pro.local>
2022-11-05 20:37:53 +0800

    finish updateFunctionComponent, updateClassComponent, updateHostTextComponent and updateFragmentComponent

--- demo/src/main.jsx
+++ demo/src/main.jsx
@@ -1,34 +1,43 @@
-import {
-  ReactDOM,
-  // Component
-} from "../which-react";
+import { ReactDOM, Component } from "../which-react";
 import "./index.css";
 
-// function FunctionComponent(props) {
-//   return (
-//     <div className="border">
-//       <p>{props.name}</p>
-//     </div>
-//   );
-// }
+function FunctionComponent(props) {
+  return (
+    <div className="border">
+      <p>{props.name}</p>
+    </div>
+  );
+}
 
-// class ClassComponent extends Component {
-//   render() {
-//     return (
-//       <div className="border">
-//         <h3>{this.props.name}</h3>
-//         this is text
-//       </div>
-//     );
-//   }
-// }
+class ClassComponent extends Component {
+  render() {
+    return (
+      <div className="border">
+        <h3>{this.props.name}</h3>
+        this is text
+      </div>
+    );
+  }
+}
+
+function FragmentComponent() {
+  return (
+    <ul>
+      <>
+        <li>fragment 1</li>
+        <li>fragmentt 2</li>
+      </>
+    </ul>
+  );
+}
 
 const jsx = (
   <div className="border">
     <h1>react</h1>
     <a href="https://github.com/yuyi2583/mini-react">mini react</a>
-    {/* <FunctionComponent name="function component" />
-    <ClassComponent name="class component" /> */}
+    <FunctionComponent name="function component" />
+    <ClassComponent name="class component" />
+    <FragmentComponent />
   </div>
 );
 
--- demo/which-react.js
+++ demo/which-react.js
@@ -1,9 +1,10 @@
 // import React, { useReducer, Component } from "react";
 // import ReactDOM from "react-dom/client";
 import ReactDOM from "../src/react-dom";
+import { Component } from "../src/react";
 
 export {
   ReactDOM,
   //  useReducer,
-  //  Component
+  Component,
 };
